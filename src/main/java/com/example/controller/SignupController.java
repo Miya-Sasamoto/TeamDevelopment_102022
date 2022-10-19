2378f793e71f6993b6878474fe1b71c13aa1cd31
sasakisora <109976240+sasakisora@users.noreply.github.com>
2022-10-20 01:42:28 +0900

    10/20 update  sasaki

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -6,67 +6,54 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype. Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation. Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.application.service.UserApplicationService;
 import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.UserService;
 import com.example.form.SignupForm;
 
-import lombok.extern.slf4j.Slf4j;
 
-@Controller 
-@RequestMapping("/user") 
-@Slf4j
+@Controller
 public class SignupController{
 	
-	@Autowired
-	private UserApplicationService userApplicationService;
 	
 	@Autowired
 	private UserService userService;
 	
 	@Autowired
-	private private ModelMapper modelMapper;
+	private  ModelMapper modelMapper;
 	
-	@GetMapping("/signup") 
-	public String getSignup(Model model, Locale locale, 
-	@ModelAttribute SignupForm form) { 
-
+	/**出勤登録画面を表示*/
+	@GetMapping("/") 
+	public String getSignup(Model model, Locale locale) { 
+		System.out.println("test");
+		return "attendance";
 	} 
 	
 	
-	/**ユーザー登録処理*/
+	/**出勤登録処理*/
 	@PostMapping("/signup") 
-	public String postSignup(Model model, Locale locale, 
-	@ModelAttribute @Validated(GroupOrder.class) SignupForm 
-	form, 
-	BindingResult bindingResult) { 
-		
-		//入カチェック結果
-		if (bindingResult.hasErrors()) {
-			//NG：ユーザー登録画面に戻ります
-			return getSignup(model, locale, form);
-		}
-		
-		log.info(form.toString());
+	public String postSignup(Model model,Locale locale,@ModelAttribute SignupForm form)
+		 { 
 		
 		
 		//formをMUserクラスに変換
-		MUser user = modelMapper.map(form, MUser.class); 
 		
-		//ユーザー登録
-		userService. signup(user);
+		MUser attendance = modelMapper.map(form,MUser.class); 
 		
-		//ログイン画面にリダイレクト
-		return "redirect:/login";
+		//出勤登録
+		userService. signup(attendance);
+		
+		//マイページor完了画面にリダイレクト
+		//完了画面作る
+		return "attendance";
+		
 	}
 }
+	
+
 
 
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,23 @@
 CREATE TABLE IF NOT EXISTS Employee ( 
-id SERIAL(8) PRIMARY KEY, 
+user_id SERIAL PRIMARY KEY, 
 name VARCHAR(50), 
 name_kana VARCHAR(50),
 password VARCHAR(250),
-mail_address VARCHAR(50),
+mail_address VARCHAR(50)
 );
 
 /*出勤*/
 CREATE TABLE IF NOT EXISTS Attendance ( 
-user_id VARCHAR(8) PRIMARY KEY FOREIGN KEY,
-date DATE(8), 
-status VARCHAR(2) FOREIGN KEY,
-start_time TIME(8),
-end_time TIME(8),
-break_time TIME(8),
+user_id integer PRIMARY KEY ,
+start_date DATE, 
+end_date DATE,
+status VARCHAR(2),
+start_time TIME(6),
+end_time TIME(6),
+break_time TIME(6),
 note VARCHAR(200),
+FOREIGN KEY (user_id)
+REFERENCES Employee (user_id)
 );
 
 
