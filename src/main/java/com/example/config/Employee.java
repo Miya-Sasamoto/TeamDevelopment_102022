2a0cea0fa55582a72daade013c6f054d3dfca638
naosugahara <105054689+naosugahara@users.noreply.github.com>
2022-10-23 00:48:34 +0900

    10/23 須賀原　レビュー

---
+++ src/main/java/com/example/config/Employee.java
@@ -0,0 +1,8 @@
+package com.example.config;
+import lombok.Data;
+@Data
+public class Employee {
+private String employeeld; 
+private String employeeName; 
+private int employeeAge;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO Employee (id, name, name_kana, password, mail_address) 
-VALUES('00000001','ｔａｒｏｕ', 'タロウ'，'00000001','tarou@example.com');
+INSERT INTO Employee (name, name_kana, password, mail_address) 
+VALUES('ｔａｒｏｕ','タロウ', '00000001', 'tarou@example.com');
 
 INSERT INTO Attendance(
 user_id,
@@ -11,4 +11,4 @@ end_time,
 break_time,
 note
 )
-VALUES('00000001','2022-01-01','2022-01-01','出勤','09:00','18:00','01:00','良好'）
+VALUES('00000001','2022-01-01','2022-01-01','出勤','09:00','18:00','01:00','良好')
