b07ab4af4f6fe6e79fc9c15f03c80e6da0984cb6
haruki-nakagomi <0520naharu@gmail.com>
2022-10-29 13:02:36 +0900

    実装　10/29

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,53 +1,55 @@
 package com.example.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        //静的リソースをセキュリティ対象外に設定
-        web.ignoring().antMatchers("/webjars/**", "/css/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                //「login.html」はログイン不要でアクセス可能に設定
-                .antMatchers("/login1").permitAll()
-                //上記以外は直リンク禁止
-                .anyRequest().authenticated()
-            .and()
-            .formLogin()
-                //ログイン処理のパス
-                .loginProcessingUrl("/login1")
-                //ログインページ
-                .loginPage("/login1")
-                //ログインエラー時の遷移先 ※パラメーターに「error」を付与
-                .failureUrl("/login?error")
-                //ログイン成功時の遷移先
-                .defaultSuccessUrl("/Mypage1", true)
-                //ログイン時のキー：名前
-                .usernameParameter("name")
-                //ログイン時のパスワード
-                .passwordParameter("password")
-            .and()
-            .logout()
-                //ログアウト時の遷移先 POSTでアクセス
-                .logoutSuccessUrl("/login1");
-    }
-
-    //パスワードのアルゴリズムをBCryptに設定
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(login -> login
+                .loginProcessingUrl("/login")
+                .loginPage("/login")
+                .defaultSuccessUrl("/Mypage1",true)
+                .failureUrl("/login?error")
+                .usernameParameter("id")
+                .passwordParameter("pass")
+                .permitAll()
+                
+        ).authorizeHttpRequests(authz -> authz
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .mvcMatchers("/shinki").permitAll()
+                .mvcMatchers("/register_completion").permitAll()
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
+
+   
+    
+  
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    
+    /** 認証の設定 
+    @Override
+    protected void configure(AuthenticationManegerBuilder auth) throws Exception{
+    	
+    	
+    	
+    	PasswordEncoder encoder = passwordEncoder();
+        auth
+        
+            .userDetailsService(userDetailsService)
+            .oasswordEncoder(encoder);
+    
+    }*/
 }
--- src/main/java/com/example/domain/user/model/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.example.domain.user.model;
-
-public interface UserService {
-	/**ユーザー登録*/
-	public void signup(MUser user); 
-}
