36e6c02671c2aec8e6c95b56737c37064be43895
Miya Sasamoto <miya.19991225@gmail.com>
2022-11-05 13:49:26 +0900

    config changes

--- src/main/java/com/example/TeamDevelopment102022Application.java
+++ src/main/java/com/example/TeamDevelopment102022Application.java
@@ -1,11 +1,9 @@
 package com.example;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "hoge.fuga") 
 public class TeamDevelopment102022Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/config/JavaConfig.java
+++ src/main/java/com/example/config/JavaConfig.java
@@ -1,13 +1,11 @@
 package com.example.config;
 
 import org.modelmapper.ModelMapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@MapperScan("org.mybatis.spring.attendance.mapper")
 public class JavaConfig {
 	@Bean
     ModelMapper modelMapper(){
