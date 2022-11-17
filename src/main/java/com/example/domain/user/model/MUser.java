ed04db24f404dbef67eb28f75fdfaba001e12c2b
naosugahara <105054689+naosugahara@users.noreply.github.com>
2022-11-17 22:36:51 +0900

    Revert "11/17"
    This reverts commit c9501b9230b67be67f7e734d7a585b99cbf0491c.

---
+++ src/main/java/com/example/domain/user/model/MUser.java
@@ -0,0 +1,13 @@
+package com.example.domain.user.model;
+
+import lombok.Data;
+@Data
+public class MUser{
+private String userId;
+private String name;
+private String nameKana;
+private String mailAddress;
+private String passWord; 
+
+
+}
